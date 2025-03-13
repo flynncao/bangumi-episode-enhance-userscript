0d0c63ddf5804f0ff1040b15bb6177545cc7dc8d
Flynn Cao <realflynncao@gmail.com>
2025-03-13 21:42:11 +0800

    refactor: 根据排序类型进行排序，优化快排

--- src/modules/comments.js
+++ src/modules/comments.js
@@ -1,10 +1,13 @@
-import { BGM_EP_REGEX, BGM_GROUP_REGEX } from '../constants/index'
+import { BGM_EP_REGEX } from '../constants/index'
 import { purifiedDatetimeInMillionSeconds } from '../utils/index'
 export default function processComments(userSettings) {
-  const username = $('.idBadgerNeue .avatar').attr('href').split('/user/')[1]
+  // check if the target element is valid
+
+  const username = $('.idBadgerNeue .avatar').attr('href')
+    ? $('.idBadgerNeue .avatar').attr('href').split('/user/')[1]
+    : ''
   const conservedPostID =
     $(location).attr('href').split('#').length > 1 ? $(location).attr('href').split('#')[1] : null
-  const isTopicPage = BGM_GROUP_REGEX.test(location.href)
   const allCommentRows = $('.row.row_reply.clearit')
   let plainCommentsCount = 0
   let featuredCommentsCount = 0
@@ -18,21 +21,23 @@ export default function processComments(userSettings) {
     const content = $(row)
       .find(BGM_EP_REGEX.test(location.href) ? '.message.clearit' : '.inner')
       .text()
-    let commentScore = 1
-    let mentionedInSubReply = false
-    const stickyMentioned = Boolean(userSettings.stickyMentioned)
+
+    let commentScore = 0
     // prioritize @me comments on
     const highlightMentionedColor = '#ff8c00'
-    if (userSettings.stickyMentioned && that.find('.avatar').attr('href').includes(username)) {
+    const subReplyContent = that.find('.topic_sub_reply')
+    const commentsCount = subReplyContent.find('.sub_reply_bg').length
+    const mentionedInMainComment =
+      userSettings.stickyMentioned &&
+      that.find('.avatar').attr('href').split('/user/')[1] === username
+    let mentionedInSubReply = false
+    if (mentionedInMainComment) {
       that.css('border-color', highlightMentionedColor)
       that.css('border-width', '1px')
       that.css('border-style', 'dashed')
       commentScore += 10000
     }
     that.find(`.topic_sub_reply .sub_reply_bg.clearit`).each(function (index, element) {
-      that.find('span.num').each(function (index, element) {
-        commentScore += Number.parseInt($(element).text())
-      })
       if (userSettings.stickyMentioned && $(element).attr('data-item-user') === username) {
         $(element).css('border-color', highlightMentionedColor)
         $(element).css('border-width', '1px')
@@ -41,15 +46,14 @@ export default function processComments(userSettings) {
         mentionedInSubReply = true
       }
     })
+    const important = mentionedInMainComment || mentionedInSubReply
     that.find('span.num').each(function (index, element) {
       commentScore += Number.parseInt($(element).text())
     })
     const hasConservedReply = conservedPostID && that.find(`#${conservedPostID}`).length > 0
     if (hasConservedReply) conservedRow = row
-    const subReplyContent = that.find('.topic_sub_reply')
     if (!hasConservedReply) subReplyContent.hide()
     const timestampArea = that.find('.action').first()
-    const commentsCount = subReplyContent.find('.sub_reply_bg').length
     if (commentsCount !== 0) {
       const a = $(
         `<a class="expand_all" href="javascript:void(0)" style="margin:0 3px 0 5px;"><span class="ico ico_reply">展开(+${commentsCount})</span></a>`,
@@ -63,12 +67,13 @@ export default function processComments(userSettings) {
     }
     // check if this comment meets the requirement of minimumContentLength
     const isShortReply = content.trim().length < minimumContentLength
-    let isFeatured = !(featuredCommentsCount >= userSettings.maxFeaturedComments)
-    if (isShortReply || commentScore <= 1) {
+    let isFeatured =
+      userSettings.sortMode === 'reactionCount' ? commentScore >= 1 : commentsCount >= 1
+    if (isShortReply || featuredCommentsCount >= userSettings.maxFeaturedComments) {
       isFeatured = false
     }
     // conserved reply must be fixed
-    if (hasConservedReply) {
+    if (hasConservedReply || important) {
       isFeatured = true
     }
 
@@ -90,6 +95,7 @@ export default function processComments(userSettings) {
         score: commentScore,
         commentsCount,
         timestampNumber: purifiedDatetimeInMillionSeconds(timestamp),
+        important,
       })
     } else {
       plainCommentsCount++
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
+// quickSort is not strictly needed cause JavaScript has built-in sort method based on quicksort/selection algorithm
 export function quickSort(arr, sortKey, changeCompareDirection = false) {
   if (arr.length <= 1) {
     return arr
@@ -6,13 +7,23 @@ export function quickSort(arr, sortKey, changeCompareDirection = false) {
   const left = []
   const right = []
   for (let i = 1; i < arr.length; i++) {
-    const compareResult = !changeCompareDirection
-      ? arr[i][sortKey] > pivot[sortKey]
-      : arr[i][sortKey] < pivot[sortKey]
-    if (arr[i][sortKey] && compareResult) {
-      left.push(arr[i])
+    const element = arr[i]
+    const elementImportant = element.important || false
+    const pivotImportant = pivot.important || false
+    let compareResult
+
+    if (elementImportant !== pivotImportant) {
+      compareResult = elementImportant // true if element is important and pivot is not
+    } else if (changeCompareDirection) {
+      compareResult = element[sortKey] < pivot[sortKey]
     } else {
-      right.push(arr[i])
+      compareResult = element[sortKey] > pivot[sortKey]
+    }
+
+    if (compareResult) {
+      left.push(element)
+    } else {
+      right.push(element)
     }
   }
   return quickSort(left, sortKey, changeCompareDirection).concat(
@@ -20,7 +31,20 @@ export function quickSort(arr, sortKey, changeCompareDirection = false) {
     quickSort(right, sortKey, changeCompareDirection),
   )
 }
-
 export function purifiedDatetimeInMillionSeconds(timestamp) {
   return new Date(timestamp.trim().replace('- ', '')).getTime()
+}
+
+export function defaultSort(arr, sortKey, changeCompareDirection = false) {
+  return arr.sort((a, b) => {
+    const aImportant = a.important || false
+    const bImportant = b.important || false
+    if (aImportant !== bImportant) {
+      return aImportant ? -1 : 1
+    }
+    if (changeCompareDirection) {
+      return a[sortKey] - b[sortKey]
+    }
+    return b[sortKey] - a[sortKey]
+  })
 }
