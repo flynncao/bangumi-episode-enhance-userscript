d4195b362383444056647e443dd298fd9ce6b5f8
Flynn Cao <realflynncao@gmail.com>
2026-07-27 20:18:45 +0800

    feat(constants): extend BGM_GROUP_REGEX to support blog and subject topics
    Update the regex to match blog and subject topic URLs in addition to group topics, and use named capture groups for easier data extraction.

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
 export const BGM_EP_REGEX = /^https:\/\/(((fast\.)?bgm\.tv)|(chii\.in)|(bangumi\.tv))\/ep\/\d+/
 export const BGM_GROUP_REGEX
-  = /^https:\/\/(((fast\.)?bgm\.tv)|(chii\.in)|(bangumi\.tv))\/group\/topic\/\d+/
+  = /^https:\/\/(?:fast\.)?(?:bgm\.tv|chii\.in|bangumi\.tv)\/(?:blog\/(?<blogId>\d+)|(?<topicType>group|subject)\/topic\/(?<topicId>\d+))/
 export const NAMESPACE = 'bangumi_comment_enhance'
